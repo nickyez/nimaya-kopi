ead26ccf34b50858bdb2c4f6892008bb2575e1ef
NickyErlanggaZ <nickyerlanggaz@gmail.com>
2022-05-28 06:07:26 +0700

    add: font awesome

--- resources/views/templates/page.blade.php
+++ resources/views/templates/page.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/1578dbcc4d.js" crossorigin="anonymous"></script>
     @stack('style')
 </head>
 <body>
